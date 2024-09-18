22f3e30f36f125d93bc27c5c5197db55cdfd6a48
vishnuvardhan <vishnuvardhan14102002@gmail.com>
2024-09-18 20:55:13 +0530

    cors issue

--- server/server.js
+++ server/server.js
@@ -11,7 +11,6 @@ const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
-
 // Connect to DB
 connectDB();
 
@@ -21,25 +20,28 @@ const corsOptions = {
     'https://medicare-frontend-delta.vercel.app', 
     'https://medicare-frontend-c17dyc2y5-vishnus-projects-4236c2a9.vercel.app'
   ], 
-  methods: 'GET,POST,PUT,DELETE',
+  methods: 'GET,POST,PUT,DELETE,OPTIONS',
   credentials: true,
   allowedHeaders: 'Content-Type,Authorization,x-auth-token',
 };
 
-
 // Middleware
 app.use(cors(corsOptions));
 app.use(express.json());
+
+// Handle OPTIONS preflight requests
+app.options('*', cors(corsOptions));
+
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api', medicineRoutes);
 app.use('/api', salesExecutiveRoutes);
-app.use('/api',productRoutes);
-app.use('/api',userRoutes);
+app.use('/api', productRoutes);
+app.use('/api', userRoutes);
 
-app.get("/",(req,res) => {
-  res.send("Welcome to Medicare API")   
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to Medicare API");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
